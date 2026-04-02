228cabe36634a824b3c32324d4e87ede927b1e3f
Pierce <pierce.c.newel@wmich.edu>
2026-04-02 09:26:32 -0400

    added croping functionality

--- pretrained.py
+++ pretrained.py
@@ -1,10 +1,21 @@
+import numpy as np
+from PIL import Image
+
+import torch 
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+import torchvision
+import torchvision.transforms as transforms
 from torchvision import models
 from torchvision.models import VGG16_Weights
-import torch.nn as nn
-import torch.optim as optim
-from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
-import torch
+from torchvision import datasets
+
+import os
+import torchvision.transforms.functional as TF
+
+from torch.utils.data import random_split, DataLoader
 
 #pull the pretrained vgg model and change the last classifier to handle 4 classes 
 #(snow, mixed, ice, dry)
@@ -19,6 +30,39 @@ for param in model.parameters():
 for param in model.classifier.parameters():
     param.requires_grad = True
 
+class RoadDataset(datasets.ImageFolder):
+    def __getitem__(self, index):
+        # 1. Get the path, image, and label
+        path, label_idx = self.samples[index]
+        image = self.loader(path) # Loads as PIL Image
+        
+        filename = os.path.basename(path)
+        condition = self.classes[label_idx] 
+
+        width, height = image.size
+
+        if "arenacam" in filename:
+            # Car hood in image
+            image = TF.crop(image, top=700, left=1200, height=400, width=400)
+        elif "pylon_camera" in filename:
+            # No car hood in view
+            image = TF.crop(image, top=400, left=800, height=400, width=400)
+        elif width < 400:
+            pass #dont crop images that are too small
+        else:
+            #base case just in case
+            image = TF.center_crop(image, (400, 400))
+
+        # apply transforms (resize, tensor, normalize)
+        if self.transform is not None:
+            image = self.transform(image)
+
+        # Returns: (image, index of label)
+        return image, label_idx
+    
+    def get_classes(self):
+        return self.classes
+
 #the vgg model expects a specific size, i havent checked if this results in a 'correct' image of the road
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
@@ -30,7 +74,7 @@ transform = transforms.Compose([
     ])
 #ImageFolder grabs the images from your specified location and creates labels automatically. 
 # change the image folder to match yours :)
-train_dataset = datasets.ImageFolder("/media/pierce/BA79-9A67/train", transform = transform)
+train_dataset = RoadDataset("/media/pierce/BA79-9A67/train", transform = transform)
 #only train on batches of 32 images at a time, can be changed 
 train_loader = DataLoader(train_dataset, batch_size = 32, shuffle = True)
 
